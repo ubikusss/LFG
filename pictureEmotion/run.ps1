572c9b77735f334b1c09a2e83b69d69ff2d37f7d
Dinica Ion (Dini) <ion.dinica@gmail.com>
2017-03-20 09:51:30 +0200

    Update run.ps1

--- pictureEmotion/run.ps1
+++ pictureEmotion/run.ps1
@@ -6,7 +6,7 @@ if ($req_query_name)
     $name = $req_query_name 
 }
 
-$response = Invoke-RestMethod -Uri "https://westus.api.cognitive.microsoft.com/emotion/v1.0/recognize" -Body "{""url"":""$url""}" -Method Post -Headers @{"Content-Type" = "application/json"; "Ocp-Apim-Subscription-Key" = "eec504b7ca5346c6aea1579cf4b2ab84"} 
+$response = Invoke-RestMethod -Uri "https://westus.api.cognitive.microsoft.com/emotion/v1.0/recognize" -Body "{""url"":""$url""}" -Method Post -Headers @{"Content-Type" = "application/json"; "Ocp-Apim-Subscription-Key" = "XXXXXXX"} 
 $return = $response | ConvertTo-Json
 
 Set-Content $outputTable $return
